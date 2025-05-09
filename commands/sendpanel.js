7245f7d6ec73ae53430308385e4df9e6e2979d4b
CrystaL <80718023+CodingWithCrystaL@users.noreply.github.com>
2025-05-10 04:38:07 +0530

    Update sendpanel.js

--- commands/sendpanel.js
+++ commands/sendpanel.js
@@ -9,7 +9,7 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setTitle('<:emoji_24:1370474663821447361> GRANDX EXCHANGE PANEL <:emoji_24:1370474663821447361>')
       .setColor('#e600f8')
-      .setDescription(`**INR TO CRYPTO**:\n<:emoji_23:1370473294448558210> Any Amount = ₹92/$\n\n**CRYPTO TO INR**:\n<:emoji_25:1370476212656537822> Below $50 = ₹86.5/$\n<:emoji_25:1370476212656537822> $50–$150 = ₹87/$\n<:emoji_25:1370476212656537822> Above $150 = ₹87.5/$\n\nPROCEED TO EXCHANGE:`);
+      .setDescription(`**INR TO CRYPTO**\n<:emoji_23:1370473294448558210>  ₹92 per $ (Any Amount)\n\n**CRYPTO TO INR**\n<:emoji_25:1370476212656537822> Below $50  = ₹86.5/$\n<:emoji_25:1370476212656537822> $50–$150  = ₹87/$\n<:emoji_25:1370476212656537822> Above $150 = ₹87.5/$`);
 
     const row = new ActionRowBuilder().addComponents(
       new StringSelectMenuBuilder()
